23a825d0fd6cb66d3ddf0d425102067f7dea4fbf
TSB <tobishittabey@gmail.com>
2024-06-02 14:42:22 +0100

    TDA: fix: trx check logic

--- functions/src/trx/retreive.ts
+++ functions/src/trx/retreive.ts
@@ -8,6 +8,7 @@ export const handleTRXRetreive = async (req: Request, res: Response) => {
     .doc(`TRX/trx:00:${id}`)
     .get()
     .then((doc: any) => {
+      if (!doc.exists) return res.status(404).json({ message: "Not found" });
       const data = doc.data();
       const trak = JSON.parse(data.serialized_trak);
       return doc.exists && trak.TRAK.trak.youtube.url
